7e9f2d60f2336a07089ad3d3336f78a31dc38dda
Rikbek <Rikibeklin23@gmail.com>
2026-05-27 19:58:01 +0700

    scripts/deploy.ts

---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Token = await ethers.getContractFactory("LitVM");
+
+  const token = await Token.deploy();
+
+  await token.waitForDeployment();
+
+  console.log("Token deployed to:", await token.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
